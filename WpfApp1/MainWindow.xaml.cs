c57951f0d6dfa7e8d303206e1aa0e1baf60d5d7a
vixzaaa <voxhyla@gmail.com>
2024-03-02 01:01:45 +0300

    Работа Пивоварова для ИТ

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) //ВыйтиX
+        {
+            this.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e) //Готово
+        {
+
+        }
+
+
+
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            AlgoritmFrame.Navigate(new Page1());
+            AlgoritmLineyni.Navigate(new Page2());
+        }
+    }
+}
